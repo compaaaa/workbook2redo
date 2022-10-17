ab52b49b07e766769c21caea3d747b42631ab673
compaaaa <azi.varela2003@gmail.com>
2022-10-17 13:36:10 -0400

    math scripts

---
+++ wb2-exercises/MathScripts/area_of_rectangle.js
@@ -0,0 +1,7 @@
+var length = 6;
+var width = 9;
+var area = length * width;
+
+console.log(
+    "The area of the rectangle is " + area
+);
---
+++ wb2-exercises/MathScripts/net_worth.js
@@ -0,0 +1,7 @@
+var assets = 0;
+var debts = 0;
+var netWorth = assets + debts;
+
+console.log(
+    "Your net worth is $" + netWorth
+);
